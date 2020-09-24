18bfdb6e2a428c75bfb5a969b93bd2b12fec3366
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2020-09-24 13:40:04 -0400

    Unity: Transform Demo

---
+++ TransformDemo/Assets/MoveCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // timer += 1;
+        timer += Time.deltaTime;
+        Debug.Log("Time passed: " + timer);
+
+        transform.Translate(Vector3.forward);
+        Debug.Log("z: " + transform.position.z);
+
+    }
+
+}
