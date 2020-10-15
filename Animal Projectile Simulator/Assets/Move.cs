cf2232262d14662cd5c5c60e596cfbc567db14d1
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2020-10-15 13:48:49 -0400

    Animal Projectile

---
+++ Animal Projectile Simulator/Assets/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    Rigidbody rb;
+    bool needsToJump;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            needsToJump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (needsToJump)
+        {
+            rb.AddForce(Vector3.up * 300.0f);
+            needsToJump = false;
+        }
+    }
+
+}
---
+++ Animal Projectile Simulator/Assets/Rotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    float rotationPower;
+    Vector3 bottomPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottomPosition = new Vector3(transform.position.x, GetComponent<Renderer>().bounds.min.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationPower = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(bottomPosition, new Vector3(0, 0, -1), rotationPower);
+    }
+
+}
