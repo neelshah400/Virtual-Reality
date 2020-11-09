d4aaf08e175f994043264c9a9e395f73e810eb0d
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2020-11-09 18:29:18 -0500

    Animations

---
+++ AnimationPractice/Assets/CubeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    private Animation a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animation>();
+        a["SpinCube"].speed = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (a != null)
+            {
+                a.Play("SpinCube");
+            }
+        }
+    }
+
+}
