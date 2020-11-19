76803b5a1e1bf1ece8ffa5ddf15291430455af95
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2020-11-19 13:40:32 -0500

    Finish Catch Game

--- Catch Game/Assets/PrefabController.cs
+++ Catch Game/Assets/PrefabController.cs
@@ -6,12 +6,14 @@ public class PrefabController : MonoBehaviour
 {
 
     GameController gameController;
+    ParticleSystem childParticleSystem;
     Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        childParticleSystem = transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
         rb = GetComponent<Rigidbody>();
         rb.AddForce(Vector3.down * 7.0f, ForceMode.Impulse);
     }
@@ -19,16 +21,15 @@ public class PrefabController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!childParticleSystem.IsAlive())
+            Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Catcher")
-        {
-            gameController.IncreaseScore();
-            Destroy(gameObject);
-        }
+        gameController.IncreaseScore();
+        transform.localScale = Vector3.zero;
+        childParticleSystem.Stop();
     }
 
     private void OnBecameInvisible()
