7883d3d64027f72c876549392d3d0964989ebacb
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2020-12-07 15:22:52 -0500

    Work on Top Down Shooter

--- Top Down Shooter/Assets/ShipController.cs
+++ Top Down Shooter/Assets/ShipController.cs
@@ -5,29 +5,42 @@ using UnityEngine;
 public class ShipController : MonoBehaviour
 {
 
-    Collider c;
-    Vector3 minXScreenPos;
-    Vector3 maxXScreenPos;
+    Collider col;
+    Vector3 minPos, maxPos;
     float mPower;
-    float rPower;
+    Quaternion rot, sRot, lRot, rRot;
     
     // Start is called before the first frame update
     void Start()
     {
-        c = GetComponent<Collider>();
+
+        col = GetComponent<Collider>();
+
+        sRot = transform.rotation;
+        lRot = Quaternion.Euler(sRot.eulerAngles.x, sRot.eulerAngles.y, sRot.eulerAngles.z + 45.0f);
+        rRot = Quaternion.Euler(sRot.eulerAngles.x, sRot.eulerAngles.y, sRot.eulerAngles.z - 45.0f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        minXScreenPos = Camera.main.WorldToScreenPoint(new Vector3(c.bounds.min.x, transform.position.y, transform.position.z));
-        maxXScreenPos = Camera.main.WorldToScreenPoint(new Vector3(c.bounds.max.x, transform.position.y, transform.position.z));
+
+        minPos = Camera.main.WorldToScreenPoint(new Vector3(col.bounds.min.x, transform.position.y, transform.position.z));
+        maxPos = Camera.main.WorldToScreenPoint(new Vector3(col.bounds.max.x, transform.position.y, transform.position.z));
         mPower = Input.GetAxis("Horizontal") * Time.deltaTime * 20.0f;
-        rPower = (float)(Input.GetAxis("Horizontal") * -45.0 - transform.eulerAngles.z);
-        if (minXScreenPos.x < 0 && mPower < 0 || maxXScreenPos.x > Screen.width && mPower > 0)
+        if (minPos.x < 0 && mPower < 0 || maxPos.x > Screen.width && mPower > 0)
             mPower = 0;
-        transform.Translate(Vector3.right * mPower);
-        transform.Rotate(0, 0, rPower);
+        transform.Translate(Vector3.right * mPower, Space.World);
+
+        if (Input.GetAxis("Horizontal") < 0.0f)
+            rot = lRot;
+        else if (Input.GetAxis("Horizontal") > 0.0f)
+            rot = rRot;
+        else
+            rot = sRot;
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * 3.0f);
+
     }
 
 }
---
+++ Top Down Shooter/Assets/ShipController2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController2 : MonoBehaviour
+{
+
+    float rPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rPower = Input.GetAxis("Horizontal") * Time.deltaTime * 100.0f;
+        transform.Rotate(0, 0, -1.0f * rPower, Space.Self);
+        Debug.Log(rPower);
+    }
+
+}
