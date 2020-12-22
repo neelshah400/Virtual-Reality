53c8c01e31ed6f57a239c736ee00b22b2907a6c8
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2020-12-21 21:13:08 -0500

    Lighting practice

---
+++ LightingPractice/Assets/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject prefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnLightSphere", 0.0f, 0.4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnLightSphere()
+    {
+        float x = Random.Range(-4.0f, 4.0f);
+        float z = Random.Range(-4.0f, 4.0f);
+        Instantiate(prefab, new Vector3(x, 10.0f, z), Quaternion.identity);
+    }
+
+}
---
+++ LightingPractice/Assets/LightSphereController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSphereController : MonoBehaviour
+{
+
+    Renderer r;
+    Light l;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        r = GetComponent<Renderer>();
+        l = transform.GetChild(0).gameObject.GetComponent<Light>();
+
+        Color color = Random.ColorHSV();
+        r.material.SetColor("_Color", color);
+        l.color = color;
+        l.intensity = Random.Range(0.5f, 5.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.localScale = Vector3.zero;
+        Invoke("DestroySphere", 2.0f);
+    }
+
+    void DestroySphere()
+    {
+        Destroy(gameObject);
+    }
+
+}
