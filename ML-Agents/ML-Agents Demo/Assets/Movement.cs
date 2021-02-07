f9f8fbcb70de9d1a0e64b7b97c0e02a6db49e0c7
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2021-02-07 14:46:07 -0500

    ML Agents Demo

---
+++ ML-Agents/ML-Agents Demo/Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+
+public class Movement : Agent
+{
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        for (int i = 0; i < vectorAction.Length; i++)
+        {
+            Debug.Log("Number: " + vectorAction[i] + " Position " + i);
+        }
+        transform.Translate(new Vector3(vectorAction[0], 0, vectorAction[1]) * Time.deltaTime);
+    }
+
+}
