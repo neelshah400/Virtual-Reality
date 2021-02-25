9f876d6b4e8c34e68a04f9e68752007962736c63
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2021-02-25 17:15:30 -0500

    Finish ML Agents Demo Part 1

---
+++ ML-Agents/ML-Agents Demo/Assets/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject prefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Instantiate(prefab, new Vector3(0.0f, i * 1.5f, 0.0f), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
--- ML-Agents/ML-Agents Demo/Assets/Movement.cs
+++ ML-Agents/ML-Agents Demo/Assets/Movement.cs
@@ -2,17 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
 
 public class Movement : Agent
 {
 
+    public Transform food;
+    int count;
+
+    public override void OnEpisodeBegin()
+    {
+
+        count = 1000;
+
+        Transform[] items = new Transform[] { transform, food };
+        foreach (Transform item in items)
+        {
+            item.localPosition = GetRandomVector(6.0f);
+            item.GetComponent<Rigidbody>().freezeRotation = true;
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
+    }
+
+    public Vector3 GetRandomVector(float d)
+    {
+        float x = Random.Range(-d, d);
+        float y = 0.5f;
+        float z = Random.Range(-d, d);
+        return new Vector3(x, y, z);
+    }
+
     public override void OnActionReceived(float[] vectorAction)
     {
-        for (int i = 0; i < vectorAction.Length; i++)
+        AddReward(-0.0005f);
+        count--;
+        if (count <= 0)
         {
-            Debug.Log("Number: " + vectorAction[i] + " Position " + i);
+            AddReward(-0.5f);
+            EndEpisode();
         }
-        transform.Translate(new Vector3(vectorAction[0], 0, vectorAction[1]) * Time.deltaTime);
+        transform.Translate(new Vector3(vectorAction[0], 0, vectorAction[1]) * Time.deltaTime * 3.0f);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(food.transform.localPosition);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (count == 1000)
+            EndEpisode();
+        Debug.Log("Hit (count = " + count + ")");
+        AddReward(1.0f);
+        EndEpisode();
     }
 
 }
